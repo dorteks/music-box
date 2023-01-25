d231b2a15b98ee1270a67af731f01d98918f9c20
dorteks <ayodelealayemi@gmail.com>
2023-01-25 14:55:58 +0100

    made changes to mobile menu and create playlist for mobiles

--- src/pages/create-playlist/mobilePlaylist.tsx
+++ src/pages/create-playlist/mobilePlaylist.tsx
@@ -1,16 +1,11 @@
 import {
-  Td,
-  Th,
-  Tr,
   Box,
   Text,
   Stack,
-  Table,
-  Tbody,
-  Thead,
   Button,
   HStack,
-  TableContainer,
+  Img,
+  useOutsideClick,
 } from "@chakra-ui/react";
 import Layout from "@/layouts/main";
 import { ReactElement } from "react";
@@ -19,10 +14,77 @@ import { FiDownload } from "react-icons/fi";
 import { FaPencilAlt } from "react-icons/fa";
 import { GiMusicSpell } from "react-icons/gi";
 import { GrShareOption } from "react-icons/gr";
-import { MdOutlineLibraryAdd } from "react-icons/md";
 import { BsSuitHeart, BsThreeDots } from "react-icons/bs";
+import { MdOutlineLibraryAdd } from "react-icons/md";
+import Link from "next/link";
+import React from "react";
+
+// export const PlaylistOptions = () => {
+//   const { isOpen, onToggle } = useDisclosure();
+
+//   return (
+//     <>
+//       <Button onClick={onToggle}>
+//         <BsThreeDots size="20px" />
+//       </Button>
+//       <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
+//         <Box
+//           p="40px"
+//           color="black"
+//           mt="4"
+//           bg="green.50"
+//           rounded="md"
+//           shadow="md"
+//           height="200px"
+//         >
+//           <VStack align="left" gap={5}>
+//             <FiDownload size="20px" />
+//             <Link href="/create-playlist/playlists">
+//               <BsSuitHeart size="20px" />
+//             </Link>
+//             <MdOutlineLibraryAdd size="20px" />
+//           </VStack>
+//         </Box>
+//       </Slide>
+//     </>
+//   );
+// };
+
+export const DisplayMenus = () => {
+  const ref = React.useRef(null);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  useOutsideClick({
+    ref: ref,
+    handler: () => setIsModalOpen(false),
+  });
+
+  return (
+    <>
+      {isModalOpen ? (
+        <div ref={ref} onClick={() => setIsModalOpen(false)}>
+          <HStack align="left" gap={5}>
+            <FiDownload
+              size="20px"
+              onClick={() => console.log("button clicked")}
+            />
+            <Link href="">
+              <BsSuitHeart size="20px" />
+            </Link>
+            <MdOutlineLibraryAdd size="20px" />
+          </HStack>
+        </div>
+      ) : (
+        <Button onClick={() => setIsModalOpen(true)}>
+          <BsThreeDots size="20px" />
+        </Button>
+      )}
+    </>
+  );
+};
 
 const MobilePLaylist = () => {
+  // const [display, toggleDisplay] = useState("none");
+
   return (
     <Box>
       <Stack
@@ -69,88 +131,82 @@ const MobilePLaylist = () => {
       </Stack>
 
       <Stack mt="30px" display={["flex", "flex", "none", "none", "none"]}>
-        <TableContainer>
-          <Table variant="simple">
-            <Thead>
-              <Tr>
-                <Th>#</Th>
-                <Th>Title</Th>
-                <Th>Artist</Th>
-                <Th></Th>
-                <Th>Time</Th>
-              </Tr>
-            </Thead>
+        {[
+          {
+            id: "1",
+            title: "Tikuku",
+            artist: "Candy Bleakz",
+            time: "02:55",
+          },
+          {
+            id: "2",
+            title: "Kilometre",
+            artist: "Burna Boy",
+            time: "02:33",
+          },
+          {
+            id: "3",
+            title: "Sungba (Remix) ft. Burna Boy",
+            artist: "Asake",
+            time: "03:30",
+          },
+          {
+            id: "4",
+            title: "Woman",
+            artist: "Simi",
+            time: "02:48",
+          },
+          {
+            id: "5",
+            title: "Tikuku",
+            artist: "Candy Bleakz",
+            time: "02:55",
+          },
+          {
+            id: "6",
+            title: "Kilometre",
+            artist: "Burna Boy",
+            time: "02:33",
+          },
+          {
+            id: "7",
+            title: "Sungba (Remix) ft. Burna Boy",
+            artist: "Asake",
+            time: "03:30",
+          },
+          {
+            id: "8",
+            title: "Woman",
+            artist: "Simi",
+            time: "02:48",
+          },
+        ].map((item) => {
+          return (
+            <Stack
+              key="id"
+              direction="row"
+              h="60px"
+              align="center"
+              justify="space-between"
+              pl={["10px", "20px"]}
+              pr={["10px", "30px"]}
+              pb="10px"
+            >
+              <HStack align="center">
+                <Img boxSize="50px" pr="5px" />
+                <Stack direction="column" align="left">
+                  <Text noOfLines={1}>{item.title}</Text>
+                  <Text fontWeight="hairline" fontSize="12px">
+                    {item.artist}
+                  </Text>
+                </Stack>
+              </HStack>
 
-            {[
-              {
-                id: "1",
-                title: "Tikuku",
-                artist: "Candy Bleakz",
-                time: "02:55",
-              },
-              {
-                id: "2",
-                title: "Kilometre",
-                artist: "Burna Boy",
-                time: "02:33",
-              },
-              {
-                id: "3",
-                title: "Sungba (Remix) ft. Burna Boy",
-                artist: "Asake",
-                time: "03:30",
-              },
-              {
-                id: "4",
-                title: "Woman",
-                artist: "Simi",
-                time: "02:48",
-              },
-              {
-                id: "5",
-                title: "Tikuku",
-                artist: "Candy Bleakz",
-                time: "02:55",
-              },
-              {
-                id: "6",
-                title: "Kilometre",
-                artist: "Burna Boy",
-                time: "02:33",
-              },
-              {
-                id: "7",
-                title: "Sungba (Remix) ft. Burna Boy",
-                artist: "Asake",
-                time: "03:30",
-              },
-              {
-                id: "8",
-                title: "Woman",
-                artist: "Simi",
-                time: "02:48",
-              },
-            ].map((item) => {
-              return (
-                <Tbody key="id">
-                  <Tr>
-                    <Td>{item.id}</Td>
-                    <Td>{item.title}</Td>
-                    <Td>{item.artist} </Td>
-                    <Td>
-                      <HStack gap={2}>
-                        <MdOutlineLibraryAdd size="20px" />
-                        <GrShareOption size="20px" />
-                        <FiDownload size="20px" />
-                      </HStack>
-                    </Td>
-                    <Td> {item.time} </Td>
-                  </Tr>
-                </Tbody>
-              );
-            })}
-          </Table>
-        </TableContainer>
+              {/* <PlaylistOptions /> */}
+              <DisplayMenus />
+            </Stack>
+          );
+        })}
       </Stack>
     </Box>
   );

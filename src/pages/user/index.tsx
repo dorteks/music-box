d8179ec8a844117ca4a97dde024b829129b04ff3
dorteks <ayodelealayemi@gmail.com>
2023-01-28 10:04:42 +0100

    added key props to data

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,10 +1,8 @@
-import PlaySong from "@/components/playSong";
 import { Box, Text } from "@chakra-ui/react";
 
 const User = () => {
   return (
     <Box>
-      <PlaySong />
       <Text>User</Text>
       <Text>user@demo.com</Text>
       <Text>Full Profile</Text>
